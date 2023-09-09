ec84ce5c24550b7cab35a4ca4b06389b66f25739
pedrocaah <143305220+pedrocaah@users.noreply.github.com>
2023-09-08 23:40:33 -0300

    Exercicio MOD7

---
+++ src/Paciente.java
@@ -0,0 +1,45 @@
+public class Paciente {
+
+    private String nome;
+
+    private String endereco;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getConvenio() {
+        return convenio;
+    }
+
+    public void setConvenio(String convenio) {
+        this.convenio = convenio;
+    }
+
+    private String convenio;
+    
+
+    public void cadastrarEndereco(String ruaX) {
+    }
+
+
+    public void cadastrarConvenio(String amil) {
+    }
+}
+
+
+
+
+
---
+++ src/PrimieiraClasse.java
@@ -0,0 +1,9 @@
+public class PrimieiraClasse {
+
+    public static void main (String args[]){
+        System.out.println("Pronto Socorro");
+        Paciente paciente = new Paciente();
+        paciente.cadastrarEndereco("Rua x");
+        paciente.cadastrarConvenio("Amil");
+    }
+}
